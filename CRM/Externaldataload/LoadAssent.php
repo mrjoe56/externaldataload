b83207ae75da621280b7022e3379f04b12bab2e5
aly2191 <ad2191@bioresource.nihr.ac.uk>
2023-04-17 10:56:20 +0100

    Updated assent data function to be correct

--- CRM/Externaldataload/LoadAssent.php
+++ CRM/Externaldataload/LoadAssent.php
@@ -40,9 +40,9 @@ class CRM_Externaldataload_LoadAssent
       else {
         $assentDate = date('Y-m-d', strtotime($data['assent_date']));
         if ($this->countExistingAssent($contactId, $assentDate, $data['assent_pis_version'], $data['assent_version']) == 0) {
-          $assentVersion = 'custom_' . CRM_Nihrbackbone_BackboneConfig::singleton()->getNbrAssentDataCustomField('nbr_assent_version', 'id');
-          $assentPisVersion = 'custom_' . CRM_Nihrbackbone_BackboneConfig::singleton()->getNbrAssentDataCustomField('nbr_assent_pis_version', 'id');
-          $assentStatus = 'custom_' . CRM_Nihrbackbone_BackboneConfig::singleton()->getNbrAssentDataCustomField('nbr_assent_status', 'id');
+          $assentVersion = 'custom_' . CRM_Nihrbackbone_BackboneConfig::singleton()->getAssentDataCustomField('nbr_assent_version', 'id');
+          $assentPisVersion = 'custom_' . CRM_Nihrbackbone_BackboneConfig::singleton()->getAssentDataCustomField('nbr_assent_pis_version', 'id');
+          $assentStatus = 'custom_' . CRM_Nihrbackbone_BackboneConfig::singleton()->getAssentDataCustomField('nbr_assent_status', 'id');
 
           // assent not yet on Civi - add assent to case
           $assentDate = new DateTime($data['assent_date']);
